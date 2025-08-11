dc66ee6cac168a10a242ff3fe16bdf42af0719f5
Dazbo <darren_lester@epam.com>
2025-08-11 21:55:00 +0100

    Tidying

--- .backup_rickbot-adk_20250809_085508/deployment/terraform/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "rickbot-adk-prd-terraform-state"
-    prefix = "rickbot-adk/prod"
-  }
-}
--- .backup_rickbot-adk_20250809_085508/deployment/terraform/secrets.tf
+++
@@ -1,9 +0,0 @@
-resource "google_secret_manager_secret" "dazbo_system_prompt" {
-  for_each      = local.deploy_project_ids
-  project       = each.value
-  secret_id = "dazbo-system-prompt"
-
-  replication {
-    auto {}
-  }
-}
