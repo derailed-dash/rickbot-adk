7313cd364da3790b2af3f769d3134280a8682a7d
Dazbo <derailed.dash@gmail.com>
2025-12-30 18:16:50 +0000

    Add GcsArtifactService

--- src/rickbot_agent/services.py
+++ src/rickbot_agent/services.py
@@ -2,13 +2,25 @@
 
 from functools import cache
 
-from google.adk.artifacts import InMemoryArtifactService
+from google.adk.artifacts import GcsArtifactService, InMemoryArtifactService
 from google.adk.sessions import BaseSessionService, InMemorySessionService
+
+from rickbot_utils.config import config
+from rickbot_utils.logging_utils import setup_logger
+
+logger = setup_logger(config.agent_name)
 
 
 @cache
 def get_artifact_service():
-    """Initialise and return the artifact service."""
+    """Initialise and return the artifact service. 
+    Use GcsArtifactService only if artifact_bucket is set."""
+    # use persistent artifact service if artifact_bucket is set
+    if config.artifact_bucket:
+        logger.info(f"Using GcsArtifactService with artifact bucket: {config.artifact_bucket}")
+        return GcsArtifactService(config.artifact_bucket)
+
+    logger.info("Using InMemoryArtifactService")
     return InMemoryArtifactService()
 
 
