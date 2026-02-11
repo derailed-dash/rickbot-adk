94962645cccc49d8bea2ab4201564bf159ebda1d
Dazbo <derailed.dash@gmail.com>
2026-02-11 16:56:56 +0000

    Add prod

--- deployment/terraform/service.tf
+++ deployment/terraform/service.tf
@@ -25,8 +25,7 @@ resource "google_cloud_run_v2_service" "app_staging" {
           cpu    = "1"
           memory = "1Gi"
         }
-        cpu_idle          = true
-        startup_cpu_boost = true
+        cpu_idle = true
       }
     }
 
@@ -91,9 +90,9 @@ resource "google_cloud_run_v2_service" "app_prod" {
       resources {
         limits = {
           cpu    = "1"
-          memory = "2Gi"
+          memory = "1Gi"
         }
-        # cpu_idle = false
+        cpu_idle          = true
         startup_cpu_boost = true
       }
     }
@@ -117,7 +116,13 @@ resource "google_cloud_run_v2_service" "app_prod" {
   # This lifecycle block prevents Terraform from overwriting the Cloud Run service when it's
   # updated by Cloud Run deployments outside of Terraform (e.g., via CI/CD pipelines)
   lifecycle {
-    ignore_changes = all
+    ignore_changes = [
+      client,
+      client_version,
+      template[0].containers[0].image,
+      template[0].containers[0].env,
+      template[0].scaling[0].min_instance_count,
+    ]
   }
 
   # Make dependencies conditional to avoid errors.
